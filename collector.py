7a03290154b63ecf8074c8b3ab49d2212db91ba6
a24be3b5 9803260d <a24be3b5.9803260d@gmail.com>
2019-03-05 17:57:11 -0500

    added support for rolling S3 bucket uploads

--- collector.py
+++ collector.py
@@ -6,33 +6,53 @@ import json
 import configparser
 from datetime import datetime
 
+from s3_uploader import BucketUploader
+
 
 class TwitterListener(StreamListener):
 
-    def next_filename(self):
+    def __next_filename(self):
         return self.prefix + datetime.now().strftime(
             '_%Y%m%d-%H%M%S_{fname}').format(
             fname='raw_tweets.jsonl')
 
-    def __init__(self, prefix, target_count=10):
+    def __init__(self, prefix, target_count, bucket_name=None):
         super().__init__(self)
         self.prefix = prefix
         self.target_count = target_count
         print("Starting with following params:")
         print(" * prefix: %s" % prefix)
         print(" * target_count: %d" % target_count)
-        self.outfile = open(self.next_filename(), 'w')
+        self.outfilename = self.__next_filename()
+        self.outfile = open(self.outfilename, 'w')
         self.tweet_count = 0
+        self.s3 = False
+        if bucket_name is not None:
+            print(" * s3_bucketname: %s" % bucket_name)
+            self.s3 = True
+            self.bucket = bucket_name
+
+    def close_file(self):
+        self.outfile.close()
+
+        # check to to see if we are uploading to s3
+        if self.s3:
+            # create uploader thread
+            uploader_thread = BucketUploader(
+                self.bucket,
+                self.outfilename)
+            uploader_thread.start()
 
     def on_data(self, data):
         self.outfile.write(data)
         self.tweet_count += 1
 
         if self.tweet_count >= self.target_count:
-            self.outfile.close()
-            nf = self.next_filename()
-            print("rolling new file at %s" % nf)
-            self.outfile = open(nf, 'w')
+            self.close_file()
+
+            self.outfilename = self.__next_filename()
+            print("rolling new file at %s" % self.outfilename)
+            self.outfile = open(self.outfilename, 'w')
             self.tweet_count = 0
 
         return True
@@ -41,16 +61,29 @@ class TwitterListener(StreamListener):
         print("Twitter API Access Error: %s" % status)
         exit(status)
 
+
 if __name__ == '__main__':
 
     config = configparser.ConfigParser()
     config.read('collector.cfg')
 
-    tl = TwitterListener(
-        config['io'].get('outfile'),
-        config['io'].getint('target_count'))
-
+    outfile = config['io'].get('outfile')
+    target_count = config['io'].getint('target_count')
     tracker_string = config['io'].get('tracker_string')
+
+    twitter_listener = None
+
+    # check for S3 configuration, enable if present
+    try:
+        twitter_listener = TwitterListener(
+            outfile,
+            target_count,
+            config['io'].get('s3_bucketname'))
+    except:
+        print("[!] No S3 bucket name found, running in local archive mode.")
+        twitter_listener = TwitterListener(
+            outfile,
+            target_count)
 
     auth = OAuthHandler(
                config['twitter'].get('consumer_key'),
@@ -59,6 +92,14 @@ if __name__ == '__main__':
                config['twitter'].get('access_token'),
                config['twitter'].get('access_token_secret'))
 
-    stream = Stream(auth, tl)
+    stream = Stream(auth, twitter_listener)
 
-    stream.filter(track=tracker_string)
+    try:
+        stream.filter(track=tracker_string)
+    except KeyboardInterrupt:
+        print(twitter_listener.__dict__.keys())
+        twitter_listener.close_file()
+        print("Clean shutdown successful!")
+    except:
+        raise
+
---
+++ s3_uploader.py
@@ -0,0 +1,42 @@
+import boto3
+import threading
+import os
+
+
+class BucketUploader(threading.Thread):
+    def __init__(self, bucket_name, filename):
+        threading.Thread.__init__(self)
+        self.bucket_name = bucket_name
+        self.filename = filename
+
+    def run(self):
+        blob_id = self.filename.split('/')[-1]
+        print("Uploading blob '%s' to S3 bucket '%s'." % (
+            blob_id,
+            self.bucket_name))
+        try:
+            s3 = boto3.resource('s3')
+            s3.Object(self.bucket_name, blob_id).put(
+                Body=open(self.filename, 'rb'))
+
+            os.remove(self.filename)
+        except:
+            print("An error occurred, leaving file '%s' on disk")
+        print("Upload successful; removed file: %s" % self.filename)
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    if len(sys.argv) < 3:
+        print("No input file specified.")
+        exit(-1)
+
+    output_bucket = sys.argv[1]
+    input_file = sys.argv[2]
+
+    uploader_thread = BucketUploader(output_bucket, input_file)
+    uploader_thread.start()
+
+
