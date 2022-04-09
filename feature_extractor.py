3db213d11acea5940beb21bc17381e4eab5441ea
a24be3b5 9803260d <a24be3b5.9803260d@gmail.com>
2022-04-09 17:26:56 -0400

    Added exception catch for 'tweets' that don't look as expected.

--- feature_extractor.py
+++ feature_extractor.py
@@ -21,44 +21,49 @@ def extract_to_file(tweetsfilename, featuresfilename):
 
         with open(tweetsfilename, 'r') as tweets:
             for rawtweet in tweets:
-                tweet = json.loads(rawtweet)
+                try:
+                    tweet = json.loads(rawtweet)
 
-                # extract common features
-                common_features = dict()
-                common_features['id'] = tweet['id']
-                common_features['timestamp_ms'] = tweet['timestamp_ms']
-                common_features['user_id'] = tweet['user']['id']
+                    # extract common features
+                    common_features = dict()
+                    common_features['id'] = tweet['id']
+                    common_features['timestamp_ms'] = tweet['timestamp_ms']
+                    common_features['user_id'] = tweet['user']['id']
 
-                # extract user relationships
-                for user in tweet['entities']['user_mentions']:
-                    write_feature_row(writer, common_features, 'mention', user['id'], 'user_id')
+                    # extract user relationships
+                    for user in tweet['entities']['user_mentions']:
+                        write_feature_row(writer, common_features, 'mention', user['id'], 'user_id')
 
-                # if not retweet extract user content
-                if "retweeted_status" not in tweet:
-                    for hashtag in tweet['entities']['hashtags']:
-                        write_feature_row(writer, common_features, 'tweet', hashtag['text'], 'hashtag')
-                    for url in tweet['entities']['urls']:
-                        write_feature_row(writer, common_features, 'tweet', url['expanded_url'], 'url')
-                    for url in tweet['entities']['urls']:
-                        tld = tldextract.extract(url['expanded_url'])
-                        write_feature_row(writer, common_features, 'tweet', tld.fqdn, 'tld')
-                    for s in tweet['entities']['symbols']:
-                        write_feature_row(writer, common_features, 'tweet', s, 'symbol')
+                    # if not retweet extract user content
+                    if "retweeted_status" not in tweet:
+                        for hashtag in tweet['entities']['hashtags']:
+                            write_feature_row(writer, common_features, 'tweet', hashtag['text'], 'hashtag')
+                        for url in tweet['entities']['urls']:
+                            write_feature_row(writer, common_features, 'tweet', url['expanded_url'], 'url')
+                        for url in tweet['entities']['urls']:
+                            tld = tldextract.extract(url['expanded_url'])
+                            write_feature_row(writer, common_features, 'tweet', tld.fqdn, 'tld')
+                        for s in tweet['entities']['symbols']:
+                            write_feature_row(writer, common_features, 'tweet', s, 'symbol')
 
-                else: # extract retweet relationships
-                    u = str(tweet['retweeted_status']['user']['id'])
-                    write_feature_row(writer, common_features, 'retweet', u, 'user_id')
+                    else: # extract retweet relationships
+                        u = str(tweet['retweeted_status']['user']['id'])
+                        write_feature_row(writer, common_features, 'retweet', u, 'user_id')
 
-                    # extract echoed content
-                    for hashtag in tweet['retweeted_status']['entities']['hashtags']:
-                        write_feature_row(writer, common_features, 'retweet', hashtag['text'], 'hashtag')
-                    for url in tweet['retweeted_status']['entities']['urls']:
-                        write_feature_row(writer, common_features, 'retweet', url['expanded_url'], 'url')
-                    for url in tweet['retweeted_status']['entities']['urls']:
-                        tld = tldextract.extract(url['expanded_url'])
-                        write_feature_row(writer, common_features, 'retweet', tld.fqdn, 'tld')
-                    for s in tweet['retweeted_status']['entities']['symbols']:
-                        write_feature_row(writer, common_features, 'retweet', s, 'symbol')
+                        # extract echoed content
+                        for hashtag in tweet['retweeted_status']['entities']['hashtags']:
+                            write_feature_row(writer, common_features, 'retweet', hashtag['text'], 'hashtag')
+                        for url in tweet['retweeted_status']['entities']['urls']:
+                            write_feature_row(writer, common_features, 'retweet', url['expanded_url'], 'url')
+                        for url in tweet['retweeted_status']['entities']['urls']:
+                            tld = tldextract.extract(url['expanded_url'])
+                            write_feature_row(writer, common_features, 'retweet', tld.fqdn, 'tld')
+                        for s in tweet['retweeted_status']['entities']['symbols']:
+                            write_feature_row(writer, common_features, 'retweet', s, 'symbol')
+
+                except:
+                    print(f"Error extracting from the following tweet:")
+                    print(rawtweet)
 
 def write_feature_row(w, common, action, entity, entity_type):
     tmp_dict = dict()
